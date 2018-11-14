f5d604245461f87e55f976efb6e502f2fe0cdb31
Kyle Davis <KyleDavisGIS@gmail.com>
2018-11-14 15:33:00 -0800

    Update PostgreSQL Field Update Script.py

--- PostgreSQL Field Update Script.py
+++ PostgreSQL Field Update Script.py
@@ -12,10 +12,10 @@ def iter_row(cursor, size=10):
             yield row
 
 # connect to postgresql db
-conn = psycopg2.connect(host="10.1.5.10", database="design_tool", user="postgres", password="postgres")
+conn = psycopg2.connect(host="host.address", database="design_tool", user="username", password="password")
 
 cur = conn.cursor()
-query = "select fqn_id, workorderid, outputports FROM design_tool.sanfrancisco.equipment WHERE outputports IS NULL and isdeleted = 'f' ORDER BY workorderid ASC"
+query = "select fqn_id, workorderid, outputports FROM design_tool.city.equipment WHERE outputports IS NULL and isdeleted = 'f' ORDER BY workorderid ASC"
 # execute sql query
 cur.execute(query)
 print("Number of features with null values in outputports column:", cur.rowcount)
@@ -23,7 +23,7 @@ for row in iter_row(cur, 10):
     print(row)
 
 # execute sql update
-cur.execute("update design_tool.sanfrancisco.equipment set outputports = 432 WHERE outputports IS NULL and isdeleted = 'f'",(432))
+cur.execute("update design_tool.city.equipment set outputports = 432 WHERE outputports IS NULL and isdeleted = 'f'",(432))
 # commit updates to database
 conn.commit()
 # terminate cursor and close connection to DB
