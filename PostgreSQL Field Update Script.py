dcab5060b5db625787afe38c94686656742164b6
Kyle Davis <KyleDavisGIS@gmail.com>
2018-11-14 15:31:19 -0800

    Update PostgreSQL Field Update Script.py

--- PostgreSQL Field Update Script.py
+++ PostgreSQL Field Update Script.py
@@ -1,7 +1,8 @@
-
+# Kyle Davis
+# Created Aug 15, 2018
 import psycopg2
 
-# create row iterator function to display results
+# create row iterator function to display query results
 def iter_row(cursor, size=10):
     while True:
         rows = cursor.fetchmany(size)
@@ -15,8 +16,9 @@ conn = psycopg2.connect(host="10.1.5.10", database="design_tool", user="postgres
 
 cur = conn.cursor()
 query = "select fqn_id, workorderid, outputports FROM design_tool.sanfrancisco.equipment WHERE outputports IS NULL and isdeleted = 'f' ORDER BY workorderid ASC"
+# execute sql query
 cur.execute(query)
-print("Number of null values in outputports:", cur.rowcount)
+print("Number of features with null values in outputports column:", cur.rowcount)
 for row in iter_row(cur, 10):
     print(row)
 
