d299e905e4e3d5887df8318e4c752805e525251e
Kyle Davis <KyleDavisGIS@gmail.com>
2018-11-14 15:29:12 -0800

    Add files via upload

---
+++ PostgreSQL Field Update Script.py
@@ -0,0 +1,30 @@
+
+import psycopg2
+
+# create row iterator function to display results
+def iter_row(cursor, size=10):
+    while True:
+        rows = cursor.fetchmany(size)
+        if not rows:
+            break
+        for row in rows:
+            yield row
+
+# connect to postgresql db
+conn = psycopg2.connect(host="10.1.5.10", database="design_tool", user="postgres", password="postgres")
+
+cur = conn.cursor()
+query = "select fqn_id, workorderid, outputports FROM design_tool.sanfrancisco.equipment WHERE outputports IS NULL and isdeleted = 'f' ORDER BY workorderid ASC"
+cur.execute(query)
+print("Number of null values in outputports:", cur.rowcount)
+for row in iter_row(cur, 10):
+    print(row)
+
+# execute sql update
+cur.execute("update design_tool.sanfrancisco.equipment set outputports = 432 WHERE outputports IS NULL and isdeleted = 'f'",(432))
+# commit updates to database
+conn.commit()
+# terminate cursor and close connection to DB
+cur.close()
+conn.close()
+
